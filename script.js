5f126b3f96367118332e87d055c542b864bfe01c
daxhoDev <developer.daxho@gmail.com>
2025-04-06 20:35:49 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,56 @@
 const inputNombre = document.getElementById('nombre');
 const inputCantidad = document.getElementById('cantidad');
+const formulario = document.getElementById("formulario");
 const btnAgregar = document.getElementById('agregar');
 const listaGastos = document.getElementById('lista-gastos');
 const vacio = document.getElementById('vacio');
 const spanGasto = document.getElementById('cifra-total');
-const btnEliminar = document.getElementById('eliminar-gasto');
+const btnEliminar = document.getElementById('eliminar');
 
-const arrGastos = [];
+let arrGastos = [];
 
-btnAgregar.addEventListener('click', e => {
-  e.preventDefault();
+const calcularTotal = function() {
+  return arrGastos.reduce((acc, gasto) => acc + gasto.cantidad, 0);
+}
 
-  if (inputNombre.value && inputCantidad.value) {
-    arrGastos.push({
-      id: arrGastos.length,
-      nombre: inputNombre.value,
-      cantidad: Number(inputCantidad.value),
-      html: `<li class="gasto"><p>${this.nombre}: <span class="cifra">${
-        this.cantidad
-      }</span> CUP</p><button onclick="${eliminarGasto(
-        this.id
-      )}" class="eliminar-gasto"><img src="close-icon.png"></button></li>`,
-    });
+const actualizarUI = function (){
 
-    console.log(arrGastos);
-
-    listaGastos.innerHTML = '';
-    arrGastos.forEach(gasto =>
-      listaGastos.insertAdjacentHTML('afterbegin', gasto.html)
-    );
-
-    const gastoTotal = arrGastos.reduce(
-      (acc, gasto) => acc + gasto.cantidad,
-      0
-    );
-    spanGasto.textContent = gastoTotal;
-  }
-
+  listaGastos.innerHTML = '';
   inputNombre.value = '';
   inputCantidad.value = '';
-});
+  
+  arrGastos.forEach(gasto => {
+    const li = `<li class="gasto"><p>${gasto.nombre}: <span class="cifra">${
+        gasto.cantidad
+      }</span> CUP</p><button onclick="eliminarGasto(${gasto.id})" class="eliminar-gasto"><img src="close-icon.png"></button></li>`;
+      
+      listaGastos.insertAdjacentHTML('afterbegin',li);
+  });
+  spanGasto.textContent = calcularTotal();
+  
+  if (arrGastos.length === 0) {
+    listaGastos.insertAdjacentHTML('afterbegin', `<li id="vacio" class="gasto">No hay gastos registrados</li>`)
+  }
+}
 
-const eliminarGasto = function (index) {
-  arrGastos = arrGastos.filter(gasto => gasto.id !== index);
-};
+const eliminarGasto = function(id) {
+  arrGastos = arrGastos.filter(gasto => gasto.id !== id);
+  actualizarUI();
+}
+
+formulario.addEventListener('submit', e => {
+  e.preventDefault();
+  
+  const nombre = inputNombre.value;
+  const cantidad = Number(inputCantidad.value);
+  
+  if (nombre && cantidad) {
+    arrGastos.push({
+      id: Date.now(),
+      nombre,
+      cantidad,
+    });
+    
+    actualizarUI();
+  }
+});
